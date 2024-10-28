08511cc8e8d1beef335eed28eda9d28a4731164b
Laureano E. Carpio <53540798+laucarmu25@users.noreply.github.com>
2024-10-28 08:34:25 +0100

    new TK_HIA_us model

--- libreria/finals/prepare_for_screening.py
+++ libreria/finals/prepare_for_screening.py
@@ -4,30 +4,28 @@ import os
 import pandas as pd
 import numpy as np
 
-def create_folder(folder_name):
-    if not os.path.exists(folder_name):
-        os.makedirs(folder_name)
-        print(f"Folder {folder_name} created")
-    else:
-        print(f"Folder {folder_name} already exists")
 
 def split_df(df, splits,path):
     for i,df in enumerate(np.array_split(df, splits)):
         chunk_name = f"IRB__screening__chunk-{str(i)}"
         print(chunk_name, df.shape)
         outfile = chunk_name+'.csv'
-        df.to_csv(path+outfile, sep = ';')
+        del df["y"]
+        df.to_csv(path+outfile, sep = ';', index = False)
         print(f"File {outfile} saved")
 
 # Create folder for_screening
-create_folder('for_screening')
+# create_folder('for_screening')
+path = '/home/lauri/Desktop/ProtoQSAR/PROJECTS/contratas/IRB/libreria/finals/for_screening/'
+
+os.makedirs(path)
+#%%
 
 # Load csv file
 
 df = pd.read_csv('/home/lauri/Desktop/ProtoQSAR/PROJECTS/contratas/IRB/libreria/finals/IRB_library_merged_all.csv', sep = ';')
 
 # Split the df in 10000 rows chunks
-path = '/home/lauri/Desktop/ProtoQSAR/PROJECTS/contratas/IRB/libreria/finals/for_screening/'
-split_df(df, 140, path)
+split_df(df, 14, path)
 
 # %%
